205f905cecf3276a82223786cdf8417c758c824f
Ian Walker-Sperber <ianwsperber@gmail.com>
2018-10-03 17:09:02 -0700

    Differentiate between http & https requests

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,20 @@
 import * as http from 'http';
 import * as https from 'https';
 import Mitm from 'mitm';
+import { TLSSocket } from 'tls';
 import * as url from 'url';
 const debug = require('debug')('yesno');
+
+interface InterfaceProxiedRequestOptions extends http.RequestOptions {
+  proxying: boolean;
+}
 
 export function enable(): void {
   debug('Enabling intercept');
   const mitm = Mitm();
 
   mitm.on('connect', (socket, opts: any) => {
+    // const isTls: boolean = socket instanceof TLSSocket;
     debug('Connect event');
     if (opts.proxying) {
       debug('Enabling proxy.');
@@ -16,13 +22,14 @@ export function enable(): void {
     }
   });
 
-  mitm.on('request', (req: any, res) => {
-    debug('Request', req.headers.host, req.url);
-    const proxied: http.ClientRequest = https.request({
+  mitm.on('request', (req: http.IncomingMessage, res: http.ServerResponse) => {
+    const isHttps: boolean = (req.connection as any).encrypted;
+    const request = isHttps ? https.request : http.request;
+    const proxied: http.ClientRequest = request({
       host: req.headers.host,
-      path: url.parse(req.url).path,
+      path: url.parse(req.url as string).path,
       proxying: true,
-    } as any);
+    } as InterfaceProxiedRequestOptions);
 
     proxied.on('response', (pRes: http.IncomingMessage) => {
       debug('Response');
--- test/unit/yesno.spec.ts
+++ test/unit/yesno.spec.ts
@@ -1,37 +1,67 @@
-import { expect } from 'chai';
 import * as http from 'http';
 import * as https from 'https';
-import Mitm from 'mitm';
-import * as url from 'url';
 import * as yesno from '../../src';
 
 describe('yesno', () => {
-  it('should mock requests', (done) => {
+  before(() => {
     yesno.enable();
+  });
 
+  it('should mock HTTP requests', (done) => {
+    const request: http.ClientRequest = http.request({
+      host: 'www.google.com',
+      path: '/',
+    });
+
+    request.on('error', (e) => {
+      // console.error(`problem with request: ${e.message}`);
+    });
+
+    request.on('end', (uh) => {
+      // console.log('End?', uh);
+    });
+
+    request.on('response', (res) => {
+      // console.log('Response');
+      // console.log(`STATUS: ${res.statusCode}`);
+      // console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
+      res.setEncoding('utf8');
+      res.on('data', (chunk: Buffer) => {
+        // console.log(`BODY: ${chunk}`);
+      });
+      res.on('end', () => {
+        // console.log('No more data in response.');
+        done();
+      });
+    });
+
+    request.end();
+  });
+  it('should mock HTTPS requests', (done) => {
     const request: http.ClientRequest = https.request({
       host: 'www.google.com',
       path: '/',
     });
 
     request.on('error', (e) => {
-      console.error(`problem with request: ${e.message}`);
+      // console.error(`problem with request: ${e.message}`);
     });
 
     request.on('end', (uh) => {
-      console.log('End?', uh);
+      // console.log('End?', uh);
     });
 
     request.on('response', (res) => {
-      console.log('Response');
-      console.log(`STATUS: ${res.statusCode}`);
-      console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
+      // console.log('Response');
+      // console.log(`STATUS: ${res.statusCode}`);
+      // console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
       res.setEncoding('utf8');
       res.on('data', (chunk: Buffer) => {
-        console.log(`BODY: ${chunk}`);
+        // console.log(`BODY: ${chunk}`);
       });
       res.on('end', () => {
-        console.log('No more data in response.');
+        // console.log('No more data in response.');
+        done();
       });
     });
 
