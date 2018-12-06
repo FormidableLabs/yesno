5d102ed4bc4b956b3ac52bcb57cf1bee7a50ae5a
Michael P. Scott <mscottx88@gmail.com>
2018-12-05 17:01:20 -0800

    feat(#27) export Recording

--- src/index.ts
+++ src/index.ts
@@ -6,5 +6,7 @@ export { IFileOptions, ISaveOptions } from './file';
 export { IInterceptOptions } from './interceptor';
 export { ISerializedHttp } from './http-serializer';
 
+export { default as Recording, IRecordingOptions, RecordMode } from './recording';
+
 export const yesno = new YesNo(new Context());
 export default yesno;
